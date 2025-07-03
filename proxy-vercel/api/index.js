c8ecec54febb284bce14328d59fc869ab729d7b7
archerchp <microtrabajo.c.archer@gmail.com>
2025-07-02 23:34:38 -0400

    Add files via upload

--- proxy-vercel/api/index.js
+++ proxy-vercel/api/index.js
@@ -1,24 +1,20 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    const urlDestino = 'https://script.google.com/macros/s/AKfycbz8SJUNOUZQDDEl595LspcMTWyhR7FYjqUy2f_ZENYutUhXqz3ho8Rzts9redJU6KUf/exec';
+
     try {
-      const response = await fetch(
-        'https://script.google.com/macros/s/AKfycbz8SJUNOUZQDDEl595LspcMTWyhR7FYjqUy2f_ZENYutUhXqz3ho8Rzts9redJU6KUf/exec',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(req.body)
-        }
-      );
+      const respuesta = await fetch(urlDestino, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(req.body)
+      });
 
-      const result = await response.json();
-      return res.status(200).json(result);
-
+      const datos = await respuesta.json();
+      res.status(200).json(datos);
     } catch (error) {
-      return res.status(500).json({ error: '❌ Error al reenviar al Apps Script', detalle: error.message });
+      res.status(500).json({ error: '❌ Error al reenviar datos al script: ' + error.message });
     }
   } else {
-    res.status(405).json({ error: 'Método no permitido' });
+    res.status(405).send('Esta función solo acepta solicitudes POST');
   }
 }
